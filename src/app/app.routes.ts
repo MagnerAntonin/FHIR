548d2c2b8adbb355d06684527af20cdfb50db40e
Victor-Cazal <vctorcazal59@gmail.com>
2025-05-22 10:48:17 +0200

    Amélioration du router et page d'acceuil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
-import { NgModule } from '@angular/core'
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { AppointmentDetailsComponent} from './features/appointment/appointment-details/appointment-details.component';
@@ -8,9 +7,9 @@ import { AppointmentListComponent} from './features/appointment/appointment-list
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, title: 'Home' },
-  { path: '**', component: NotFoundComponent, title: 'Page not found'},
   { path: 'details', component : AppointmentDetailsComponent, title: 'Détails'},
   { path: 'form', component : AppointmentFormComponent, title: 'Formulaire'},
-  { path: 'list', component : AppointmentListComponent, title: 'Liste'}
+  { path: 'list', component : AppointmentListComponent, title: 'Liste'},
+  { path: '**', component: NotFoundComponent, title: 'Page not found'}
 ];
 
