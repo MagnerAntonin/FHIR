dfaaf686726c05b0d4edd4014f5a47d829d29c7b
J-b-benetti <j.b.benetti81@gmail.com>
2025-05-27 10:20:12 +0200

    Add schedule & slot model.ts

---
+++ src/app/services/schedule.model.ts
@@ -0,0 +1,24 @@
+export class Schedule {
+    resourceType = 'Schedule';
+    actor: { reference: string; display?: string }[];
+    planningHorizon: { start: string; end: string };
+    comment?: string;
+
+    constructor(
+        practitionerId: string,
+        locationId: string,
+        start: string,
+        end: string,
+        comment?: string
+    ) {
+        this.actor = [
+            { reference: `Practitioner/${practitionerId}` },
+            { reference: `Location/${locationId}` },
+        ];
+        this.planningHorizon = { start, end };
+
+        if (comment) {
+            this.comment = comment;
+        }
+    }
+}
---
+++ src/app/services/slot.model.ts
@@ -0,0 +1,19 @@
+export class Slot {
+    resourceType = 'Slot';
+    schedule: { reference: string };
+    status: 'busy' | 'free' | 'busy-unavailable' | 'busy-tentative';
+    start: string;
+    end: string;
+
+    constructor(
+        scheduleId: string,
+        start: string,
+        end: string,
+        status: 'busy' | 'free' | 'busy-unavailable' | 'busy-tentative' = 'free'
+    ) {
+        this.schedule = { reference: `Schedule/${scheduleId}` };
+        this.status = status;
+        this.start = start;
+        this.end = end;
+    }
+}
